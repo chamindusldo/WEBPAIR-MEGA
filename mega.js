7a1580c5b6663683e4e091d4fc220b90bff1f83a
chamindusldo <168992075+chamindusldo@users.noreply.github.com>
2025-01-10 09:19:46 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'sadiyamin7@gmail.com', // Replace with your Mega email
-    password: 'mega@2006', // Replace with your Mega password
+    email: 'chamindulakshan46@gmail.com', // Replace with your Mega email
+    password: 'chamindu123', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
